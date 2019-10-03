629e115e1cda57264db9c5a642528ad9a204d6a4
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-03 15:27:56 -0400

    Completed basic react conversion from handlebars.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../stylsheets/App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Mexico City Airbnb App here
+      </div>
+    )
+  }
+}
